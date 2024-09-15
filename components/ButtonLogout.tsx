682e6c08e2abc20fd9c9ffb9f62f38bbebfe8b73
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-09-15 09:30:04 -0300

    login y logout del lado del cliente

--- components/ButtonLogout.tsx
+++ components/ButtonLogout.tsx
@@ -1,28 +1,13 @@
-// "use client"
-
-// import { Button } from "@/components/ui/button"
-// import { logoutAction } from "@/actions/logoutAction"
-
-// const ButtonLogout = () => {
-//   const handleClick = async () => {
-
-//       await logoutAction()
-    
-//   }
-
-//   return (
-//     <Button type="button" onClick={handleClick} className="bg-[#791b6b] ">
-//       Logout
-//     </Button>
-//   )
-// }
-
-// export default ButtonLogout
-
 "use client"
-import { signOut } from "next-auth/react"
+import { signOut } from "next-auth/react" // del lado del client
 import { Button } from "@/components/ui/button"
- 
-export function ButtonLogout() {
-  return <Button onClick={() => signOut()}>Sign Out</Button>
+
+export const ButtonLogout = () => {
+  const handleClick = async () => {
+    await signOut({
+      callbackUrl: "/login",
+      
+    })
+  }
+  return <Button onClick={handleClick} className="bg-[#791b6b]">Sign Out</Button>
 }
