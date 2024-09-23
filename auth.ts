4a50e2c72a2f34484fe4d68481fd1435c26d9acf
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-09-22 23:13:53 -0300

    create role

--- auth.ts
+++ auth.ts
@@ -9,5 +9,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   ...authConfig,
   session: { strategy: "jwt" },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.role = user.role; // agrega informacion adicional al token
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user.role= token.role as string; // expone la informacion de el token en la sesion
+      return session
+    },
+  },
 })
 
