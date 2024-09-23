c9b23e70d7306b772075a87b31b0969629c91e7c
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-09-22 23:44:55 -0300

    create role

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,8 +1,31 @@
+import { auth } from "@/auth"
+import { ButtonLogout } from "@/components/ButtonLogout";
 
-const AdminPage = () => {
+
+const AdminPage = async() => {
+  const session = await auth();
+
+  if (session?.user?.role !== 'ADMIN') {
+    return (
+      <div>
+        <h1>Access Denied, You are not an administrator </h1>
+      </div>
+    )
+
+  }
+    
   return (
-    <div>page</div>
-  )
-}
+    <div className="relative z-10">
+      <div className="container">
+        {/* session, filtro, identación */}
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+      <div>
+        <ButtonLogout />
+      </div>
+    </div>
+  );
+};
+
 
 export default AdminPage
