9b349255946c3deea404a8e001155f9bb0e02eb0
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-10-01 16:24:31 -0300

    feat(verification): email of verification

--- auth.config.ts
+++ auth.config.ts
@@ -1,47 +1,64 @@
+import Credentials from "@auth/core/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import { loginSchema } from "@/validators/login.zod";
+import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
+import {nanoid} from "nanoid";
 
-import Credentials from "@auth/core/providers/credentials"
-import type { NextAuthConfig } from "next-auth"
-import { loginSchema } from "@/validators/login.zod"
-import { prisma } from "@/prisma"
-import bcrypt from "bcryptjs"
- 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-    providers: [
-        Credentials({
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        const { data, success } = loginSchema.safeParse(credentials);
 
-          authorize: async (credentials) => {
+        if (!success) {
+          throw new Error("Invalid credentials");
+        }
 
-            const { data, success } = loginSchema.safeParse(credentials)
-
-
-            if (!success) {
-              throw new Error("Invalid credentials")
-            }
-
-
-            const user = await prisma.user.findUnique({
-              where: {
-                email: data.email,
-              },
-            })
-            
-            if (!user || !user.password) {
-              throw new Error("Invalid credentials")
-            }
-
-            const isValid = await bcrypt.compare(data.password, user.password) 
-            
-            if (!isValid) {
-              throw new Error("Invalid credentials")
-            }
-                    
-
-           return user
-            
-
+        const user = await prisma.user.findUnique({
+          where: {
+            email: data.email,
           },
-          
-        }),
-      ],
-} satisfies NextAuthConfig
+        });
+
+        if (!user || !user.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isValid = await bcrypt.compare(data.password, user.password);
+
+        if (!isValid) {
+          throw new Error("Invalid credentials");
+        }
+
+        if (!user.emailVerified) {
+          const verificationToken = await prisma.verificationToken.findFirst({
+            where: {
+              identifier: user.email,
+            },
+          });
+
+          if (verificationToken?.identifier) {
+            await prisma.verificationToken.delete({
+              where: {
+                identifier: user.email,
+              },
+            });
+          }
+
+          const token = nanoid();
+          await prisma.verificationToken.create({
+            data: {
+              identifier: user.email,
+              token,
+              expires: new Date(Date.now() + 60 * 60 * 1000), // 1 hour
+            },
+          })
+        }
+
+        return user;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
